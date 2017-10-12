39fd74800ff04cd808dcf9c80c69b7999ca2e808
ThibaultLatrille <thibault.latrille@ens-lyon.org>
2017-10-12 17:45:41 +0200

    Generate a graph from a tree in the newick format. Code not commented !

--- main.cpp
+++ main.cpp
@@ -8,55 +8,6 @@
 
 using namespace std;
 
-// This class represents a directed graph using adjacency list representation
-class Graph {
-
-private:
-    unsigned V;    // No. of vertices
-    vector<vector<int>> adj;    // Pointer to an array containing adjacency lists
-
-public:
-
-    Graph(const unsigned vertices) : V(vertices), adj(vertices, vector<int>()) {}  // Constructor
-
-    Graph(const Graph &) = delete; // copy constructor
-
-    void addEdge(int v, int w) {
-        adj[v].push_back(w); // Add w to v’s list.
-    } // function to add an edge to graph
-
-    void BFS(int s);  // prints BFS traversal from a given source s
-};
-
-void Graph::BFS(int s) {
-    // Mark all the vertices as not visited
-    vector<bool> visited(V, false);
-
-    // Create a queue for BFS
-    deque<int> queue;
-
-    // Mark the current node as visited and enqueue it
-    visited[s] = true;
-    queue.push_back(s);
-
-    while (!queue.empty()) {
-        // Dequeue a vertex from queue and print it
-        int f = queue.front();
-        cout << f << endl;
-        queue.pop_front();
-
-        // Get all adjacent vertices of the dequeued vertex s
-        // If a adjacent has not been visited, then mark it visited
-        // and enqueue it
-        for (auto i : adj[f]) {
-            if (!visited[i]) {
-                visited[i] = true;
-                queue.push_back(i);
-            }
-        }
-    }
-}
-
 // The nucleotides
 string nucleotides{"ATGC"};
 // Mapping nucleotides to their positions in the string 'nucleotides'
@@ -239,22 +190,22 @@ public:
             // Mutate the chosen nucleotide with the transition given by the loop break
             dna_str[index / 4] = nucleotides[index % 4];
 
-        } else if (time_left < 0.){
+        } else if (time_left < 0.) {
             time_left = 0.;
         }
         return time_left;
     }
 
     // Generate substitutions until equilibrium using the method 'next_substitution' (slow)
-    void burn_in(){
+    void burn_in() {
         double d = INFINITY;
-        for (unsigned m{0}; m < 4 * length; m++){
+        for (unsigned m{0}; m < 4 * length; m++) {
             next_substitution(d);
         }
     }
 
     // Generate substitutions until equilibrium by calculating explicitly the equilibrium (fast)
-    void burn_in_fast(){
+    void burn_in_fast() {
         for (unsigned n{0}; n < length; n++) { // For all indices of the sequence length
 
             // Initialize the total sum of transition rates at 0
@@ -300,9 +251,157 @@ public:
     string get_dna() { return dna_str; }
 };
 
+class Node {
+private:
+    string name;
+    string length;
+    string subtree;
+public:
+
+    unsigned long index;
+
+    Node(const string name, const string length, const string subtree) :
+            name(name), length(length), subtree(subtree) {
+        static unsigned long static_index{0};
+        cout  << "static_index" << static_index << endl;
+        index = static_index;
+        static_index++;
+    }  // Constructor
+    string get_subtree() {
+        return subtree;
+    }
+    string get_name() {
+        return name;
+    }
+
+};
+
+
+// This class represents a directed graph using adjacency list representation
+class Graph {
+
+private:
+    vector<vector<unsigned long>> adj;    // Pointer to an array containing adjacency lists
+    vector<Node> nodes;
+public:
+
+    Graph(const string newick){
+        Node root("ROOT", "0", newick);
+        nodes.push_back(root);
+        adj.push_back(vector<unsigned long>());
+
+        parse_newick(newick, root);
+    }  // Constructor
+
+    Graph(const Graph &) = delete; // copy constructor
+
+    void addNode(Node node, const Node & parent) {
+        nodes.push_back(node);
+        adj.push_back(vector<unsigned long>());
+
+        addEdge(parent, node);
+        if (node.get_subtree().size() > 0){
+            parse_newick(node.get_subtree(), node);
+        }
+    }
+
+    void addEdge(const Node & from, Node & to) {
+        adj[from.index].push_back(to.index); // Add w to v’s list.
+    } // function to add an edge to graph
+
+    void parse_newick(string newick, const Node & parent) {
+        unsigned long max_point{newick.size()};
+        unsigned long point{0};
+
+        while (point < max_point) {
+            string subtree{""};
+            if (newick[point] == '(') {
+
+                unsigned long postpoint{point};
+                unsigned long nbr_open{1};
+
+                for (unsigned long i{point+1}; i < max_point; i++) {
+                    if (nbr_open == 0) {
+                        postpoint = i;
+                        break;
+                    } else if (newick[i] == '(') {
+                        nbr_open++;
+                    } else if (newick[i] == ')') {
+                        nbr_open--;
+                    };
+                }
+                subtree = newick.substr(point+1, postpoint - point - 2);
+                point = postpoint;
+            }
+
+            long next_sep = newick.substr(point).find(",");
+            string name_suffix{""};
+
+            if (next_sep == -1){
+                name_suffix = newick.substr(point);
+                point = max_point;
+            } else {
+                name_suffix = newick.substr(point, (unsigned long)next_sep);
+                point = point + (unsigned long)next_sep + 1;
+            }
+
+            long ddot = name_suffix.rfind(':');
+            string length{""};
+            string name{""};
+
+            if (ddot != -1) {
+                length = name_suffix.substr((unsigned long)ddot + 1);
+                name = name_suffix.substr(0, (unsigned long)ddot);
+            } else {
+                name = name_suffix;
+            }
+            addNode(Node(name, length, subtree), parent);
+        }
+    }
+
+    void BFS(unsigned long s);  // prints BFS traversal from a given source s
+};
+
+void Graph::BFS(unsigned long s) {
+    // Mark all the vertices as not visited
+    vector<bool> visited(nodes.size(), false);
+
+    // Create a queue for BFS
+    deque<unsigned long> queue;
+
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+
+    while (!queue.empty()) {
+        // Dequeue a vertex from queue and print it
+        unsigned long f = queue.front();
+        cout << nodes[f].get_name() << endl;
+        queue.pop_front();
+
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for (auto i : adj[f]) {
+            if (!visited[i]) {
+                visited[i] = true;
+                queue.push_back(i);
+            }
+        }
+    }
+}
 
 // Driver program to test methods of graph class
 int main() {
+    string s{
+            "(Homo_sapiens:1.014243,(Canis_lupus_familiaris:0.844021,(Equus_caballus:0.805325,((Bos_taurus:0.182884,((Capra_hircus:0.011884,Capra_aegagrus:0.011884)'0.7-1.9':0.044887,Ovis_aries:0.056771)'4.6-7.0':0.126113)'13.6-25.4':0.455399,Sus_scrofa:0.638283)'63.1-64.8':0.167041)'75.6-88.1':0.038697)'79.1-92.4':0.170222)'92.5-115.2'"};
+    string newick{"(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F"};
+    string wnewick{"((F:0.3,G:0.4)A:0.5,B:0.2,(C:0.3,D:0.4)E:0.5)F"};
+
+    cout << newick << endl;
+
+    Graph g(s);
+    g.BFS(0);
 
     Sequence_dna seq(999);
     cout << seq.get_dna() << endl;
@@ -318,18 +417,6 @@ int main() {
     cout << seq.get_dna() << endl;
     cout << t << endl;
 
-    // Create a graph given in the above diagram
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-
-    cout << "Following is Breadth First Traversal "
-         << "(starting from vertex 2)\n";
-    g.BFS(2);
 
     return 0;
 }
